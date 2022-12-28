970e04d40c38f9f23cbc2d3a50318381bc3907a5
Pierre Nguyen <pierrenguyen@Pierres-MacBook-Pro.local>
2022-12-27 16:06:23 -0800

    task 3

--- index.js
+++ index.js
@@ -86,8 +86,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-  /*your code here*/
+function addFlavor(list, flavor){
+  list.unshift(flavor)
+  return list;
  }
 
 
